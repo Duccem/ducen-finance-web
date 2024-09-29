789b29f6944761b056188adfa16d2bddb39130d9
Jose Veliz <ducen29@gmail.com>
2024-09-28 23:05:41 -0400

    fix some structures

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,26 @@
+import "@/assets/global.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pocket Admin",
+  description: "Admin panel for Pocket",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${nunito.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
